c2792448646039730fe68ced80e43e7665c2d8b4
Alex <pioneerko@hotmail.com>
2022-10-03 17:23:33 +0100

    Added Postgres basic connection TODO: create login/logout functionality

--- app.py
+++ app.py
@@ -1,29 +1,11 @@
 from flask import Flask
 
 
-# MongoDB Settings
-
-# from flask_pymongo import PyMongo
-
-# ENV variables
-"""
-SECRET_KEY
-MONGO_URI
-MONGO_DBNAME
-"""
-
 app = Flask(__name__)
-
-# app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
-# app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
-
-# mongo = PyMongo(app)
 
 
 @app.route('/')
-def hello_world():  # put application's code here
-    # Example to fetch data from DB
-    # retrieve_db_list = list(mongo.db.dbname.find())
+def hello_world():
     return 'Hello World!'
 
 
