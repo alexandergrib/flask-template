f1ef458115aa75b2aea7b64d45361d7d674d1ae0
Alex <pioneerko@hotmail.com>
2022-11-26 10:59:27 +0000

    setup blank index page

--- app.py
+++ app.py
@@ -1,13 +1,19 @@
-from flask import Flask
+import os
 
+from flask import Flask, request, render_template, redirect, url_for
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+@app.route('/', methods=["GET", "POST", "PUT", "PATCH", "DELETE"])
+def index():
+    if request.method == "POST" or request.method == "PATCH" or request.method == "DELETE" or request.method == "PUT":
+        return redirect(url_for("index"))
+    else:
+        return render_template("index.html", index_page=True)
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host=os.environ.get("IP"),
+            port=os.environ.get("PORT"),
+            debug=False)
